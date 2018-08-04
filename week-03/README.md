7d4158e61362620f76d1e6019ef9f52a14a8146a
Raman D <ramandhali5@gmail.com>
2018-08-04 14:42:18 -0700

    add week 3 challanges

---
+++ week-03/README.md
@@ -0,0 +1,23 @@
+### Python Code Drills
+
+#### DAY ONE
+
+##### Exercises:
+
+1. [Getting familiar with the Terminal](day-01/01)
+
+
+
+#### DAY TWO
+
+##### Exercises:
+
+1. [More Practice with Variables](day-03/01)
+
+2. [More Practice with Variables and logic check](day-03/02)
+
+3. [More Practice with Variables, logic check, prompting, confirm ](day-03/03)
+
+4. [More Practice with Variables, logic check](day-03/04)
+
+5. [More Practice with Variables & Arrays](day-03/05)
---
+++ week-03/day-03/05/challenge-prompt.py
@@ -0,0 +1,44 @@
+# create an array1 with 0, 1 ,2 ,3
+
+# create an array2 with 4,5,6,7
+
+# create an array3 with 8,9,10,11
+
+# create an array4 with 12,13,14,15
+
+
+
+
+# print the first array1
+
+# print each index of the array1
+
+
+
+
+
+
+# print the second array2
+
+# print each index of the second array2
+
+
+
+
+
+# print the third array3
+
+# print each index of the third array3
+
+
+
+
+
+# print the fourth array4
+
+# print each index of the fourth array4
+
+
+
+
+
